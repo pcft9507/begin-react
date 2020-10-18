6c8a3f9833a9acdb54ade39173f00ede2805908a
DESKTOP-L1063TR\cool- <shinyx55@gmail.com>
2020-10-19 02:36:03 +0900

    Updates

---
+++ precache-manifest.fbb38fa4b8d02d83c562895e7d3f4330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a976e5074d554c55efe9637174aeccb",
+    "url": "/pcft9507/begin-react/index.html"
+  },
+  {
+    "revision": "3c4ed80bd9ec1f9f04e0",
+    "url": "/pcft9507/begin-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3e1b75195cccb56a8cd9",
+    "url": "/pcft9507/begin-react/static/js/2.5a9c7695.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pcft9507/begin-react/static/js/2.5a9c7695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4ed80bd9ec1f9f04e0",
+    "url": "/pcft9507/begin-react/static/js/main.b55a9628.chunk.js"
+  },
+  {
+    "revision": "4ceffca5462fddf681be",
+    "url": "/pcft9507/begin-react/static/js/runtime-main.44edb8da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/begin-react/precache-manifest.4533f25829d88cc168fcd657e51d9de1.js"
+  "/pcft9507/begin-react/precache-manifest.fbb38fa4b8d02d83c562895e7d3f4330.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/begin-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pcft9507/begin-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
